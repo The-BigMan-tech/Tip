82bae6fdd01abeff9504ffbd0b065c9f567607b4
The-BigMan-tech <saddlebox9@gmail.com>
2024-11-11 18:30:28 +0100

    I dont want to mess up

--- src/components/left-comp.js
+++ src/components/left-comp.js
@@ -1,14 +1,18 @@
 'use client'
 import tw from 'tailwind-styled-components'
+import styled from 'styled-components'
+import {useState,useEffect,createContext} from 'react'
+import RightComponent from './right-comp'
 
+//Dont forget about component did mount 
 const Left = tw.div`
     flex flex-col absolute left-10 top-5 text-lg
 `
 const Styledinput = tw.input`
-    bg-[#d9e5ec] mt-3 h-10 w-[21rem] text-end font-bold
+    bg-[#d9e5ec] mt-3 h-10 w-[21rem] text-end font-bold text-[#2f676b] text-2xl focus:bg-white focus:outline focus:outline-2 focus:outline-[#befff8]
 `
 const Styledtip = tw.div`
-    flex flex-col relative top-9 text-lg mb-7
+    flex flex-col relative top-9 text-lg mb-7 focus:border-green-200
 `
 const Tips = tw.div`
     flex w-96 flex-wrap mt-4 gap-x-7 gap-y-4
@@ -19,20 +23,38 @@ const Tipbutton = tw.button`
 const Custombutton = tw(Tipbutton)`
     bg-[#f0f7f9] text-[#697f7f] hover:border-2 hover:border-[#5aa79d] hover:bg-white
 `
+
+
+export const Totalbill = createContext('')
 export default function LeftComponent() {
+    let [bill,setBill] = useState('')
+    let [total_amount,setTotalamount] = useState(0.00)
+    
+    let [tip,setTip] = useState(0)
+    function watchbill(event) {
+        setBill(event.target.value)
+    }
+    function submitbill(event) {
+        (event.key == 'Enter')?setTotalamount(bill):null
+    }
+    function submitTip(event) {
+        let num = event.target.textContent.slice(0,-1)
+        setTip((num/100) * total_amount)
+    }
+    
     return(
         <>
             <Left>
                 <h1>Bill</h1>
-                <Styledinput type="text" />
+                <Styledinput onChange={watchbill} onKeyDown = {submitbill} type="text" value={bill}/>
                 <Styledtip>
                     <h1>Select tip</h1>
                     <Tips>
-                        <Tipbutton>5%</Tipbutton>
-                        <Tipbutton>10%</Tipbutton>
-                        <Tipbutton>15%</Tipbutton>
-                        <Tipbutton>25%</Tipbutton>
-                        <Tipbutton>30%</Tipbutton>
+                        <Tipbutton onClick={submitTip}>5%</Tipbutton>
+                        <Tipbutton onClick={submitTip}>10%</Tipbutton>
+                        <Tipbutton onClick={submitTip}>15%</Tipbutton>
+                        <Tipbutton onClick={submitTip}>25%</Tipbutton>
+                        <Tipbutton onClick={submitTip}>30%</Tipbutton>
                         <Custombutton>Custom</Custombutton>
                     </Tips>
                 </Styledtip>
@@ -40,6 +62,9 @@ export default function LeftComponent() {
                     <h1>Number of people</h1>
                     <Styledinput type="text"/>
                 </Styledtip>
+                <Totalbill.Provider value={{total_amount,setTotalamount,tip,setTip}}>
+                    <RightComponent/>
+                </Totalbill.Provider>
             </Left>
         </>
     )
--- src/components/right-comp.js
+++ src/components/right-comp.js
@@ -1,8 +1,10 @@
 'use client'
 import tw from 'tailwind-styled-components'
+import { Totalbill } from './left-comp'
+import {useContext} from 'react'
 
 const Tipbox = tw.div`
-    absolute right-6 top-5 bg-[#00474b] h-[26rem] text-white w-[22rem] border-2 border-transparent rounded-xl flex flex-col
+    relative left-96 bottom-[25rem] bg-[#00474b] h-[26rem] text-white w-[22rem] border-2 border-transparent rounded-xl flex flex-col
 `
 const TiptextFlex = tw.div`
     flex flex-col absolute top-7 left-5 gap-y-10
@@ -16,14 +18,23 @@ const Tipprice = tw.div`
 const Resetbutton = tw.div`
     bg-[#0d686d] flex items-center justify-center relative top-[22rem] h-12 w-80 left-4 border-2 border-transparent rounded-lg text-[#144f53]
 `
+const Fade = tw.div`
+    text-[#669a9d] text-sm
+`
 export default function RightComponent() {
+    let {total_amount,setTotal,tip,setTip} = useContext(Totalbill)
     return (
         <>
         <Tipbox>
             <TiptextFlex>
                 <Tiptext>
+                    <h1>Bill</h1>
+                    <Tipprice>${total_amount}</Tipprice>
+                </Tiptext>
+                <Tiptext>
                     <h1>Tip Amount</h1>
-                    <Tipprice>$0.00</Tipprice>
+                    <Fade>/person</Fade>
+                    <Tipprice>${tip}</Tipprice>
                 </Tiptext>
                 <Tiptext>
                     <h1>Total</h1>
